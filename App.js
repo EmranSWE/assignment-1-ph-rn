50bbf60e9f8a9cab17fe7338b0d6ef9d9a2992e3
EmranSWE <mdemran.swe@gmail.com>
2024-03-12 00:57:35 +0600

    [DONE] initial setup done

--- App.js
+++ App.js
@@ -1,20 +1,81 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { Image, SafeAreaView, StyleSheet, Text, View } from "react-native";
+import Icon from "react-native-vector-icons/FontAwesome";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+    <SafeAreaView style={styles.container}>
+      {/* Heading Info */}
+      <View
+        style={{
+          flexDirection: "row",
+          gap: 40,
+          justifyContent: "flex-start",
+          marginTop: 50,
+        }}
+      >
+        <Image
+          source={{
+            uri: "https://cdn-icons-png.flaticon.com/512/25/25231.png",
+          }}
+          style={styles.imageIcons}
+        />
+        <View>
+          <Text style={{ fontSize: 20, fontWeight: "bold" }}>The Octocat</Text>
+          <Text style={{ color: "blue" }}>@Octocat</Text>
+          <Text style={{ color: "gray" }}>Joined 25 Jan 2011</Text>
+        </View>
+      </View>
+      <Text style={{ color: "gray" }}>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Neque, odio!
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Neque, odio!
+      </Text>
+      <View style={{ flexDirection: "row", gap: 20 }}>
+        <View>
+          <Text style={{ color: "gray" }}>Repos</Text>
+          <Text style={{ fontWeight: "bold" }}>8</Text>
+        </View>
+        <View>
+          <Text style={{ color: "gray" }}>Followers</Text>
+          <Text style={{ fontWeight: "bold" }}>3938</Text>
+        </View>
+        <View>
+          <Text style={{ color: "gray" }}>Following</Text>
+          <Text style={{ fontWeight: "bold" }}>9</Text>
+        </View>
+      </View>
+
+      {/* Extra Information */}
+      <View >
+        <View style={{ flexDirection: "row", alignItems: "center" }}>
+          <Icon name="map-marker" size={30} color="black" />
+          <Text style={{ marginLeft: 10 }}>San Francisco</Text>
+        </View>
+        <View>
+          <Text>icons</Text>
+          <Text>https://github.blog</Text>
+        </View>
+        <View>
+          <Text>icons</Text>
+          <Text>Not Available</Text>
+        </View>
+        <View>
+          <Text>icons</Text>
+          <Text>@github</Text>
+        </View>
+      </View>
       <StatusBar style="auto" />
-    </View>
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    padding: 30,
+    borderRadius: "20",
+  },
+  imageIcons: {
+    height: 50,
+    width: 50,
   },
 });
